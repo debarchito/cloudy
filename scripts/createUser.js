4320e19f0839d42e69a435d6e963520a97af346b
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-02-19 19:45:44 +0530

    Add scripts

---
+++ scripts/createUser.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+/**
+ * Creates a user in the database.
+ * @script
+ */
+
+import { generateId } from "lucia";
+import { client } from "./deps/db.js";
+import { Argon2id } from "oslo/password";
+import { input, password } from "@inquirer/prompts";
+
+const name = await input({ message: "Enter username: " });
+const pswd = await password({ message: "Enter password: " });
+const confirmPswd = await password({ message: "Confirm password: " });
+
+if (pswd !== confirmPswd) {
+  console.error("[!] Passwords do not match! Retry.");
+  process.exit(1);
+}
+
+try {
+  const userId = generateId(16);
+  const hashedPassword = await new Argon2id().hash(pswd);
+
+  await client.execute({
+    sql: `INSERT INTO users (id, name, password) VALUES (?, ?, ?)`,
+    args: [userId, name, hashedPassword],
+  }).then(() => console.log("[+] User created successfully!"));
+} catch (err) {
+  console.log("[!] An error occurred! Additional info: \n");
+  console.error(err);
+}
---
+++ scripts/deps/db.js
@@ -0,0 +1,21 @@
+/**
+ * Lucia Auth layer setup for scripts.
+ * @script_deps
+ */
+
+import { config } from "dotenv";
+import { createClient } from "@libsql/client";
+
+// Only use the .env file in the root directory.
+config({
+  path: "../.env",
+});
+
+/**
+ * A libSQL client.
+ */
+export const client = createClient({
+  url: process.env.DATABASE_URL,
+  authToken:
+    process.env.DATABASE_LOCATION === "remote" ? process.env.DATABASE_AUTH_TOKEN : undefined,
+});
