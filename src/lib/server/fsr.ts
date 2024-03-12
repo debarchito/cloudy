cd2c2cbab4f56090e045e191613a6fe722632140
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-03-12 17:38:56 +0530

    Refactoring and documentation for fsr

--- src/lib/server/fsr.ts
+++ src/lib/server/fsr.ts
@@ -1,98 +1,61 @@
-import { eq, ne, and, sql } from "drizzle-orm";
 import * as schema from "./schema";
+import { nanoid } from "$lib/utils";
+import { lsBuilder } from "./fsr/ls";
 import type { NodePgDatabase } from "drizzle-orm/node-postgres";
 
-const { files, dirs } = schema;
+/**
+ * Represents the parameters for a file in the system.
+ */
+type FileParameters = Omit<typeof schema.files.$inferInsert, "id" | "createdAt" | "properties"> & {
+  properties: Record<string, string>;
+};
 
-type FileParameters = typeof schema.files.$inferInsert;
-type DirParameters = typeof schema.dirs.$inferInsert;
-
-type LsReturn = {
-  dirs: {
-    id: string;
-    name: string;
-    properties: {
-      [key: string]: string;
-    },
-    created_at: string;
-  }[];
-  files: {
-    id: string;
-    name: string;
-    chunk_url_array: string[];
-    properties: {
-      [key: string]: string;
-    };
-    created_at: string;
-  }[];
-}
+/**
+ * Represents the parameters for a directory in the file system.
+ */
+type DirParameters = Omit<typeof schema.dirs.$inferInsert, "id" | "createdAt" | "properties"> & {
+  properties: Record<string, string>;
+};
 
 /**
  * @description File System Representation (fsr)
  * @param db The database instance.
+ * @returns Shell-like methods to operate on the fsr.
  */
 export async function fsr(db: NodePgDatabase<typeof schema>) {
   return {
-    createFile: async function (params: FileParameters) {
-      return db.insert(files).values({ ...params, parentDirId: params.parentDirId || "root" });
+    /**
+     * @description Creates a file.
+     * @param params - The file parameters required to create a file.
+     * @returns A Promise of the details of the created file.
+     */
+    async touch(params: FileParameters) {
+      const id = nanoid(128);
+      return db
+        .insert(schema.files)
+        .values({ ...params, id })
+        .returning();
     },
-    createDir: async function (params: DirParameters) {
-      return db.insert(dirs).values({ ...params, parentDirId: params.parentDirId || "root" });
+
+    /**
+     * @description Creates a directory.
+     * @param params - The directory parameters required to create a directory.
+     * @returns A Promise of the details of the created directory.
+     */
+    async mkdir(params: DirParameters) {
+      const id = nanoid(128);
+      return db
+        .insert(schema.dirs)
+        .values({ ...params, id })
+        .returning();
     },
-    shell: {
-      ls: async function (userId: string, parentDirId = "root") {
-        return await db.execute<LsReturn>(await sql`
-          SELECT
-            jsonb_build_object(
-              'dirs', jsonb_agg(jsonb_build_object(
-                'id', directory_id,
-                'name', directory_name,
-                'properties', directory_properties,
-                'created_at', directory_created_at
-              )) FILTER (WHERE directory_name IS NOT NULL),
-                'files', jsonb_agg(jsonb_build_object(
-                    'id', file_id,
-                    'name', file_name,
-                    'properties', file_properties,
-                    'chunk_url_array', file_chunk_url_array,
-                    'created_at', file_created_at
-                )) FILTER (WHERE file_name IS NOT NULL)
-            ) AS result
-            FROM (
-              SELECT
-                d.id AS directory_id,
-                d.name AS directory_name,
-                d.properties AS directory_properties,
-                d.created_at AS directory_created_at,
-                null AS file_id,
-                null AS file_name,
-                null AS file_properties,
-                null AS file_chunk_url_array,
-                null AS file_created_at
-              FROM
-                dirs d
-              WHERE
-                d.parent_dir_id = ${parentDirId}
-                AND d.id != 'root'
-                AND d.user_id = ${userId}
-                UNION ALL
-              SELECT
-                null AS directory_id,
-                null AS directory_name,
-                null AS directory_properties,
-                null AS directory_created_at,
-                f.id AS file_id,
-                f.name AS file_name,
-                f.properties AS file_properties,
-                f.chunk_url_array AS file_chunk_url_array,
-                f.created_at AS file_created_at
-              FROM
-                files f
-              WHERE
-                f.parent_dir_id = ${parentDirId}
-                AND f.user_id = ${userId}
-          ) AS combined_results`);
-      },
-    },
+
+    /**
+     * @description Retrieves the list of directories and files within a specified parent directory for a given user.
+     * @param userId - The user ID.
+     * @param parentDirId - The parent directory ID.
+     * @returns A Promise of the list of directories and files.
+     */
+    ls: lsBuilder(db),
   };
 }
---
+++ src/lib/server/fsr/ls.ts
@@ -0,0 +1,85 @@
+import { sql } from "drizzle-orm";
+import * as schema from "$lib/server/schema";
+import type { NodePgDatabase } from "drizzle-orm/node-postgres";
+
+/**
+ * @description Represents the return type of the `ls` function.
+ */
+type LsReturn = {
+  dirs: {
+    id: string;
+    name: string;
+    properties: Record<string, string>;
+    created_at: string;
+  }[];
+  files: {
+    id: string;
+    name: string;
+    chunk_url_array: string[];
+    properties: Record<string, string>;
+    created_at: string;
+  }[];
+};
+
+/**
+ * @description Builds the `ls` function that retrieves the list of directories and files within a specified parent directory for a given user.
+ * @param db - The database instance.
+ * @returns A function that accepts the `userId` and `parentDirId` (`root` is default), and returns a Promise of the list of directories and files.
+ */
+export function lsBuilder(db: NodePgDatabase<typeof schema>) {
+  return async function (userId: string, parentDirId = "root") {
+    return await db.execute<LsReturn>(
+      await sql`
+      SELECT
+        jsonb_build_object(
+          'dirs', jsonb_agg(jsonb_build_object(
+            'id', directory_id,
+            'name', directory_name,
+            'properties', directory_properties,
+            'created_at', directory_created_at
+          )) FILTER (WHERE directory_name IS NOT NULL),
+            'files', jsonb_agg(jsonb_build_object(
+                'id', file_id,
+                'name', file_name,
+                'properties', file_properties,
+                'chunk_url_array', file_chunk_url_array,
+                'created_at', file_created_at
+            )) FILTER (WHERE file_name IS NOT NULL)
+        ) AS result
+        FROM (
+          SELECT
+            d.id AS directory_id,
+            d.name AS directory_name,
+            d.properties AS directory_properties,
+            d.created_at AS directory_created_at,
+            null AS file_id,
+            null AS file_name,
+            null AS file_properties,
+            null AS file_chunk_url_array,
+            null AS file_created_at
+          FROM
+            dirs d
+          WHERE
+            d.parent_dir_id = ${parentDirId}
+            AND d.id != 'root'
+            AND d.user_id = ${userId}
+            UNION ALL
+          SELECT
+            null AS directory_id,
+            null AS directory_name,
+            null AS directory_properties,
+            null AS directory_created_at,
+            f.id AS file_id,
+            f.name AS file_name,
+            f.properties AS file_properties,
+            f.chunk_url_array AS file_chunk_url_array,
+            f.created_at AS file_created_at
+          FROM
+            files f
+            WHERE
+            f.parent_dir_id = ${parentDirId}
+            AND f.user_id = ${userId}
+      ) AS combined_results`,
+    );
+  };
+}
