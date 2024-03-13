54a5c9b8f05054a935f936bc2140e69aa6a1ee3d
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-03-13 17:51:33 +0530

    Remove auto-matic migration for now

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,6 +1,5 @@
 import pg from "pg";
 import * as schema from "./schema";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { DATABASE_URL } from "$env/static/private";
 import { drizzle } from "drizzle-orm/node-postgres";
 
@@ -15,8 +14,3 @@ export const pool = new pg.Pool({
  * The database instance.
  */
 export const db = drizzle(pool, { schema });
-
-// Run migrations
-migrate(db, {
-  migrationsFolder: "./drizzle",
-});
