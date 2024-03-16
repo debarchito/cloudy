39d876cfb1861ad668a82b43d56c98a667850f98
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-03-16 23:04:00 +0530

    First version of search

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
   import { Input } from "$lib/components/ui/input";
+  import { Toggle } from "$lib/components/ui/toggle";
+  import { Search, Regex, CaseSensitive } from "lucide-svelte";
 </script>
 
-<div>
-  <Input type="search" placeholder="Search..." class="h-9 md:w-[100px] lg:w-[300px]" />
+<div class="flex w-full max-w-sm items-center space-x-2 rounded-lg border border-input px-3 pr-0">
+  <Search class="h-10 w-10 pr-3 text-primary" />
+  <Input type="search" class="h-11 border-none md:w-[100px] lg:w-[300px]" />
+  <Toggle>
+    <Regex class="h-4 w-4" />
+  </Toggle>
+  <Toggle>
+    <CaseSensitive class="h-5 w-5" />
+  </Toggle>
 </div>
