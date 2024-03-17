bafa5b1b31d55a354b01ab6165b5650284b6c4c2
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2024-03-17 10:09:50 +0530

    Search design test v2

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -4,9 +4,14 @@
   import { Search, Regex, CaseSensitive } from "lucide-svelte";
 </script>
 
-<div class="flex w-full max-w-sm items-center space-x-2 rounded-lg border border-input px-3 pr-0">
-  <Search class="h-10 w-10 pr-3 text-primary" />
-  <Input type="search" class="h-11 border-none md:w-[100px] lg:w-[300px]" />
+<div
+  class="flex w-full max-w-sm items-center space-x-2 rounded-lg border border-input px-3 pr-0 ring-1 ring-primary"
+>
+  <Search class="h-7 w-7 pr-1 text-primary" />
+  <Input
+    type="search"
+    class="h-11 border-none focus-visible:ring-0 focus-visible:ring-offset-0 md:w-[100px] lg:w-[300px]"
+  />
   <Toggle>
     <Regex class="h-4 w-4" />
   </Toggle>
